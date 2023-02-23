ed20e3df446f0039575dd6c6b448f2b813420fa0
Gabriele Scogna <gabrysco@live.it>
2023-02-23 16:31:56 +0100

    Laravel

---
+++ primo_progetto_Gabriele_Scognamiglio/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link rel=Style href="public\Style.css"
+<title>Laravel</title>
+
+</head>
+<body>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/">LARAVEL</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/SecondaPagina">Link</a>
+<h1 class="text-danger">HELLO WORLD!!</h1>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
